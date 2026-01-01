850ba2b052ef79ba8847c556d4189e765a70f41b
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2026-01-01 16:08:23 +0530

    Update DATABASE_CHANNEL environment variable

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ HELP_TXT =  os.environ.get("HELP_MESSAGE", "⁉️ Hᴇʟʟᴏ {mention} ~\n\n <
 FSUB_PIC = os.environ.get("FSUB_PIC", "https://ibb.co/DHqBS4V7")
 FSUB_LINK_EXPIRY = 300
 LOG_FILE_NAME = "Rexbots.txt"
-DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1002761937912"))
+DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1002210669265"))
 
 logging.basicConfig(
     level=logging.INFO,
